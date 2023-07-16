0592c518a82f77d123f715e7edfe8ffb422e0377
kishor-km <kishormaski16@gmail.com>
2023-07-16 14:48:01 +0530

    added seat model in our db associate with airplanes

---
+++ src/utils/common/enum.js
@@ -0,0 +1,10 @@
+const SEAT_TYPE = {
+    BUSINESS : 'bUsiness',
+    ECONOMY  :'economy',
+    PRIMIUM_ECONOMY : 'primium-economy',
+    FIRST_CLASS  : 'first-class'
+}
+
+module.exports = {
+    SEAT_TYPE,
+}
